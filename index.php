14ee430c2b8ab1cdecb7f2f32c11fe067d3a6045
José Sut <jose.sut@galileo.edu>
2024-03-11 22:53:15 -0600

    Elaborando el ejercicio de php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $nombre = "José Sut";
+    echo "<h1>Hola mundo!, soy $nombre </h1>";
+?>
+
+<style>
+    h1{
+        padding-top: 2rem;
+    }
+
+    body{
+        display: flex;
+        align-items: center;
+        flex-direction: column; 
+    }
+</style>
+
+    <img src="./gatitos_IA.jpg" alt="Imagen gatito IA" width="900px">
